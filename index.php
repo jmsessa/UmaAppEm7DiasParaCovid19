d43761b342d7e130eda9f1cb82060f14905b7cc5
João Sessa <joaomanganzi@hotmail.com>
2020-05-03 14:55:20 +0100

    Update index.php
    Adição de novo codigo.
    logo, icone, menu.
    footer

--- index.php
+++ index.php
@@ -1,3 +1,66 @@
-<?php 
-echo "Ola Mundo, Jms";
- ?>
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <meta charset="utf-8">
+    <title>Covid-19 Angola</title>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700|Roboto+Slab:400,700|Pacifico' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="icon" href="img/logo_covid.png">
+</head>
+  
+<body>
+    
+    <nav>
+        <a href=""><h1 class="logo"> </h1>
+        <a href="index.php">Covid-19  Angola</a>
+        
+    </nav>
+
+   
+    <section id="servicos">
+        <h2>SERVIÇOS</h2>
+        <div>
+            <img src="img/sintoma1.png" alt="Engrenagens">
+            <h3>Fácil Integração</h3>
+            <p>Integra facilmente seu site a qualquer gerenciador de conteúdo.</p>
+        </div>
+        <div>
+            <img src="img/hospedagem.png" alt="Servidor">
+            <h3>Hospedagem Moderna</h3>
+            <p>Não deixe seus clientes na mão. Tenha seu site 24hrs no ar.</p>
+        </div>
+        <div>
+            <img src="img/site.png" alt="Navegador">
+            <h3>Melhores Práticas</h3>
+            <p>Código legível, desenvolvido com as melhores práticas.</p>
+        </div>
+        <div>
+            <img src="img/coracao.png" alt="Coração">
+            <h3>Feito com Amor</h3>
+            <p>É melhor fazer seus sites com amor hoje em dia!</p>
+        </div>
+    </section>
+
+    
+    <section id="contato">
+        <h2>CONTATO</h2>
+        <p>Entre em contato conosco.</p>
+        <div>
+            <img src="img/fone.png" alt="fone">
+            <p><a href="tel:935678941">(+244) 935678941</a></p>
+        </div>
+
+        <div>
+            <img src="img/contato.png" alt="email">
+            <p><a href="mailto:joaomanganzi@hotmail.com">joaomanganzi@hotmail.com</a></p>
+        </div>
+    </section>
+
+    <footer>
+        <p>Desenvolvido por JMS.</p>
+    </footer>
+    
+</body>
+</html>
